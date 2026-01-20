b0b2688f704a08078f80ebda0ef6ed770270a96a
Your Name <your.email@example.com>
2026-01-20 20:47:57 +0700

    Refine section order: Move TargetAudience below CTASection

--- app/page.tsx
+++ app/page.tsx
@@ -19,8 +19,8 @@ export default function Home() {
       <Navigation />
       <Hero />
       <PainPoints />
-      <TargetAudience />
       <CTASection />
+      <TargetAudience />
       <Introduction />
       <FourPillars />
       <Benefits />
